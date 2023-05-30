a0e6d69550c4e78178967b94fbcfbf6bb6ba05c6
abenaTenkorang <abenagy16@gmail.com>
2023-05-30 08:23:26 +0000

    Set up

---
+++ menu/select.rb
@@ -0,0 +1,27 @@
+class Select
+  def initialize
+    @app = nil
+  end
+
+  def opt_select(opt)
+    case opt
+    when 1
+      @app.add_book
+    when 2
+      @app.add_music_album
+    when 3
+      @app.add_game
+    when 4
+      @app.list_all_books
+    when 5
+      @app.list_all_music_albums
+    when 6
+      @app.list_all_games
+    when 7
+      puts 'Thanks for using this app'
+      exit
+    else
+      puts 'Option entered is invalid'
+    end
+  end
+end
