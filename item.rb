644857e31520b0954555ddf2a5511f3e1f6ff73f
abenaTenkorang <abenagy16@gmail.com>
2023-05-31 14:16:11 +0000

    Add test to game and author

--- item.rb
+++ item.rb
@@ -8,6 +8,7 @@ class Item
     @publish_date = nil
     @archived = false
     @label = nil
+    @authors = author
     @genre = nil
     @author = nil
     @source = nil
@@ -15,6 +16,11 @@ class Item
 
   def move_to_archive
     @archived = true if can_be_archived?
+  end
+
+  def add_author(author)
+    @authors = author
+    author.items << self unless author.items.include?(self)
   end
 
   private
