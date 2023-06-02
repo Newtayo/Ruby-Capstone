21f5ce3b608c64c0289776b4a9a796bd3d80f92d
Newtayo <akandeabdul@gmail.com>
2023-06-02 14:56:03 +0100

    Fix linter error

--- Book/book.rb
+++ Book/book.rb
@@ -1,10 +1,9 @@
 require_relative '../item'
 
 class Book < Item
-
   attr_accessor :publish_date, :publisher, :cover_state
 
-  def initialize(publish_date, publisher, cover_state)
+  def initialize(publisher, cover_state)
     super(publish_date)
     @publisher = publisher
     @cover_state = cover_state
